75b3187dbdeefe03a2cff4b8a358c47de663952c
Artem <artrom170@gmail.com>
2023-05-22 16:10:37 +0300

    created ByteConverter start()

--- byteconverter.cpp
+++ byteconverter.cpp
@@ -1,6 +1,16 @@
 #include "byteconverter.h"
 ByteConverter::ByteConverter(Source *source, Sink *sink):
   source_(source),
-  sink_(sink)
+  sink_(sink),
+  isRunning_(false)
 {
+}
+void ByteConverter::start()
+{
+  thread_ = std::thread(&ByteConverter::run, this);
+  isRunning_ = true;
+}
+void ByteConverter::run()
+{
+  //
 }
--- byteconverter.h
+++ byteconverter.h
@@ -1,13 +1,18 @@
 #ifndef BYTECONVERTER_H
 #define BYTECONVERTER_H
+#include <thread>
 #include "source.h"
 #include "sink.h"
 class ByteConverter
 {
 public:
   explicit ByteConverter(Source *source, Sink *sink);
+  void start();
 private:
   Source *source_;
   Sink *sink_;
+  bool isRunning_;
+  std::thread thread_;
+  void run();
 };
 #endif
