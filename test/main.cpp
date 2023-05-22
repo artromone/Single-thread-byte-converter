cd75f36aed47f647afa64338b21cd4119d1a2973
Artem <artrom170@gmail.com>
2023-05-22 21:45:05 +0300

    Coding Guidelines cleanup

--- test/main.cpp
+++ test/main.cpp
@@ -17,25 +17,33 @@ public:
 };
 TEST(ByteConverterTest, ConvertBytes)
 {
-  std::vector< std::uint8_t > inputData = {0b00001011, 0b01000100, 0b10011100};
-  std::vector< std::string > expectedOutput = {"11", "4", "Unknown type"};
+  std::vector< std::uint8_t > inputData = {0b10000011, 0b01000100, 0b10011100};
+  std::vector< std::string > expectedOutput = {"d", "4", "Unknown type"};
   MockSource source;
   MockSink sink;
+  int size = static_cast<int>(inputData.size());
   EXPECT_CALL(source, hasDataNext())
-    .Times(inputData.size())
+    .Times(size)
     .WillRepeatedly(::testing::Return(true));
-  EXPECT_CALL(source, hasDataNext())
-    .WillOnce(::testing::Return(false));
   EXPECT_CALL(source, read())
-    .Times(inputData.size())
-    .WillRepeatedly(::testing::Invoke([&]()
-                                      {
-                                        static std::size_t index = 0;
-                                        return inputData[index++];
-                                      }));
+    .Times(size)
+    .WillOnce(::testing::Return(inputData[0]))
+    .WillOnce(::testing::Return(inputData[1]))
+    .WillOnce(::testing::Return(inputData[2]));
   EXPECT_CALL(sink, writeData(::testing::_))
-    .Times(inputData.size())
-    .WillRepeatedly(::testing::Return());
+    .Times(size)
+    .WillOnce(::testing::Invoke([&](const std::string &data)
+      {
+        EXPECT_EQ(data, expectedOutput[0]);
+      }))
+    .WillOnce(::testing::Invoke([&](const std::string &data)
+      {
+        EXPECT_EQ(data, expectedOutput[1]);
+      }))
+    .WillOnce(::testing::Invoke([&](const std::string &data)
+      {
+        EXPECT_EQ(data, expectedOutput[2]);
+      }));
   ByteConverter converter(&source, &sink);
   converter.start();
   std::this_thread::sleep_for(std::chrono::milliseconds(10));
